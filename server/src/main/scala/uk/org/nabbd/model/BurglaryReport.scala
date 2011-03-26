b2a9b52a21cb55f4ca2bec13c224b3e11443e1af
Daniel Rendall <drendall@gmail.com>
2011-03-26 13:47:39 +0000

    Initial parsing of XML and a test for it.

--- server/src/main/scala/uk/org/nabbd/model/BurglaryReport.scala
+++ server/src/main/scala/uk/org/nabbd/model/BurglaryReport.scala
@@ -1,6 +1,10 @@
 package uk.org.nabbd.model
 
 import net.liftweb.mapper._
+import xml.Node
+import org.slf4j.LoggerFactory
+import java.text.SimpleDateFormat
+import java.util.Date
 
 /**
  * @todo Add some documentation!
@@ -21,5 +25,54 @@ class BurglaryReport extends LongKeyedMapper[BurglaryReport] {
 }
 
 object BurglaryReport extends BurglaryReport with LongKeyedMetaMapper[BurglaryReport] with CRUDify[Long, BurglaryReport] {
+
+  val log = LoggerFactory.getLogger(BurglaryReport.getClass);
+  val dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss")
+
+  def parseReport(aReport : Node) = {
+    val now = new Date
+    aReport match {
+      case <nabbd>{contents @ _* }</nabbd> => {
+        val burglary = contents \\ "burglary"
+        log.debug(burglary.toString)
+        val userGuid = (burglary \ "@userGuid").toString
+        val reportGuid = (burglary \ "@reportGuid").toString
+        val reportDate = dateFormat.parse((burglary \ "@reportDate").toString)
+        val details = (burglary \ "details").head
+
+        val latitude = getLocationValue(details, "latitude")
+        val longitude = getLocationValue(details, "longitude")
+        val accuracy = getLocationValue(details, "accuracy")
+
+
+        val items = (burglary \ "items").head
+
+        for (itemEl <- (items \ "item")) {
+          val name = (itemEl \ "name").text
+          val serial = (itemEl \ "serial").text
+          val smartwaterUse = (itemEl \ "smartwater" \ "@use").text
+          val smartwater = (itemEl \ "smartwater").text
+          val barcodeType = (itemEl \ "barcode" \ "@type").text
+          val barcode = (itemEl \ "barcode").text
+          val category = (itemEl \ "category").text
+          val price = (itemEl \ "price").text
+
+          log.debug("Name " + name + " Serial " + serial + " SmartwaterUse" + smartwaterUse + " Smartwater " + smartwater + " BarcodeType " + barcodeType + " Barcode " + barcode + " Price " + price)
+
+          val item = Item.create.name(name).category(category).serial(serial).smartwater(smartwater).barcode(barcode).price(price).isStolen(true).createdAt(now).save()
+        }
+
+      }
+    }
+  }
+
+  def getLocationValue(details : Node, which : String) : String = {
+      (details \ "location").head match {
+        case <location>{contents @ _*}</location> => {
+           (contents \\ which).text
+        }
+        case _ => { "" }
+      }
+  }
 
 }
