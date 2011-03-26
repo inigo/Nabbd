17dc5f935a82c1b239ab4d29c330988cecf34198
Inigo <inigo.surguy@gmail.com>
2011-03-26 16:23:19 +0000

    Added phone number API to REST interface

--- server/src/main/scala/uk/org/nabbd/RestApi.scala
+++ server/src/main/scala/uk/org/nabbd/RestApi.scala
@@ -2,9 +2,9 @@ package uk.org.nabbd
 
 import model.BurglaryReport
 import net.liftweb.http.S
-import xml.XML
 import net.liftweb.http.rest.RestHelper
 import net.liftweb.util.Mailer
+import xml.{Text, XML}
 
 /**
  * Provides a REST API for generating new data.
@@ -20,6 +20,10 @@ object RestApi extends RestHelper {
     case "api" :: "BurglaryReport" :: _ Post _ =>
       (for {report <- S.param("report") ?~ "Param 'report' is missing" }
               yield addReport(report))
+    case "api" :: "PolicePhone" :: _ Get _ =>
+      (for {lat <- S.param("lat") ?~ "Param 'report' is missing";
+            long <- S.param("long") ?~ "Param 'long' is missing" }
+              yield addReport(lat))
   }
 
   def addReport(text: String) = {
@@ -34,6 +38,11 @@ object RestApi extends RestHelper {
     } catch {
       case e: Exception => <failed>{ e.getMessage }</failed>
     }
+  }
+
+  def lookupPhoneNumber(lat: String, long: String) = {
+    val phone = PoliceFinder.lookup(lat, long).phone
+    Text(phone)
   }
 
   def sendEmail(to: String, report: BurglaryReport) = {
