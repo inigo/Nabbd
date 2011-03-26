981a8dce27fd64a578520c495d6841fd5d675af7
Daniel Rendall <drendall@gmail.com>
2011-03-26 16:47:41 +0000

    Police finder appears to work now

--- server/src/main/scala/uk/org/nabbd/PoliceFinder.scala
+++ server/src/main/scala/uk/org/nabbd/PoliceFinder.scala
@@ -1,5 +1,12 @@
 package uk.org.nabbd
 
+import java.util.Properties
+import net.liftweb.util.JSONParser._
+
+import xml.{XML, Node}
+import com.gargoylesoftware.htmlunit.WebClient
+import org.apache.http.impl.client.BasicCredentialsProvider
+
 /**
  * Find information on a given police force if provided with a latitude and longitude, using the Police API.
  *
@@ -9,10 +16,37 @@ package uk.org.nabbd
 
 object PoliceFinder {
 
+  val props = new Properties()
+  props.load(this.getClass.getResourceAsStream("/nabbd.properties"))
+  val forceDetails = XML.load(PoliceFinder.getClass.getResourceAsStream("policeLookup.xml"))
+  val policeApi = props.getProperty("police.api")
+  val policeUsername = props.getProperty("police.username")
+  val policePassword = props.getProperty("police.password")
+
+
   def lookup(lat: String, long: String): Force = {
+
+
+    val url = new java.net.URL("http://" + policeUsername + ":" + policePassword + "@" + policeApi + lat + "," + long)
+    val response = url.openConnection().getContent().toString
+
+    val json = parse(response)
+
+    log.debug(response)
+
+
+
     Force("Metropolitan","inigo.surguy@gmail.com", "0123 4567890")
   }
 
+//  private def getPage(url: String): String = {
+//    val webClient = new WebClient()
+//    val provider = new BasicCredentialsProvider()
+//    provider.s
+//    webClient.setCredentialsProvider()
+//    webClient.setJavaScriptEnabled(false)
+//    webClient.getPage(url).asInstanceOf[String]
+//  }
 }
 
 case class Force(name: String, email: String, phone: String)
